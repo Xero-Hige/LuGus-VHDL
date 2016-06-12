32f3cce8f7d36f87a02dabe79b4cf200e96ff50a
Xero-Hige <gaston.martinez.90@gmail.com>
2016-06-12 01:48:59 -0300

    Removed old folder
    Former-commit-id: df68791914212d12db1c9bed8926ae9a1e6255c6

--- Tp2-LuGus/utility.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
-package utility is
-	type bcd_vector is array (natural range <>) of std_logic_vector(3 downto 0);
-	type signal_vector is array(natural range <>) of std_logic;
-end;
