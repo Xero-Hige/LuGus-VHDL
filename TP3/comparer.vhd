782c633d2a395854b8698da7b8e3051fe4702129
Xero-Hige <gaston.martinez.90@gmail.com>
2017-04-06 18:30:38 -0300

    Updated comparer

--- TP3/comparer.vhd
+++ TP3/comparer.vhd
@@ -24,9 +24,26 @@ begin
 		variable first_g: std_logic;
 		variable second_g: std_logic;
 		variable both_eq: std_logic;
-		variable carry: std_logic;
+		variable first: integer;
+		variable second: integer;
 	begin
-		if
+		first_g := 0;
+		second_g := 0;
+		both_eq := 0;
+		first := to_integer(signed(number1_in))
+		second := to_integer(signed(number2_in))
+
+		if first > second then
+			first_g := 1;
+		elsif first < second then
+			second_g := 1;
+		else
+			both_eq := 1;
+		end if;
+
+		first_greater <= first_g;
+		second_greater <= second_g;
+		equals <= both_eq;
 	end process;
 
 end architecture;
