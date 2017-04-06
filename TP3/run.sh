a413476f873d8506a597e9f4a49036dfa641f373
Xero-Hige <gaston.martinez.90@gmail.com>
2017-04-06 20:17:43 -0300

    Fixed script missing file extension on the build script

--- TP3/run.sh
+++ TP3/run.sh
@@ -1,7 +1,7 @@
 ghdl -a number_splitter.vhd
 ghdl -a adder.vhd
 ghdl -a comparer.vhd
-ghdl -a binary_multiplexer
+ghdl -a binary_multiplexer.vhd
 # ghdl -a number_splitter_tb.vhd
 # ghdl -e tb
 # ghdl -r number_splitter_tb --vcd=out.vcd --stop-time=20us
