260da460a175e9b15f98c2f88a7e13ae174962b7
Xero-Hige <gaston.martinez.90@gmail.com>
2017-04-06 22:04:13 -0300

    Added class implemented adder

--- TP3/run.sh
+++ TP3/run.sh
@@ -2,6 +2,7 @@ ghdl -a number_splitter.vhd
 ghdl -a adder.vhd
 ghdl -a comparer.vhd
 ghdl -a binary_multiplexer.vhd
+ghdl -a class_adder.vhd
 # ghdl -a number_splitter_tb.vhd
 # ghdl -e tb
 # ghdl -r number_splitter_tb --vcd=out.vcd --stop-time=20us
