2ba436f3852c08d9ee7b324f3042d2584d4e5239
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2017-04-20 18:22:13 -0300

    Update and rename number_splitter_tb to number_splitter_tb.vhd

--- TP3/number_splitter_tb.vhd
+++ TP3/number_splitter_tb.vhd
@@ -1,12 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity number_splitter_tb is
-end;
+library vunit_lib;
+context vunit_lib.vunit_context;
 
-architecture number_splitter_tb_arch of number_splitter_tb is
+entity tb_standalone is
+	generic (runner_cfg : string := runner_cfg_default);
+end entity;
+
+architecture number_splitter_tb_arq of tb_standalone is
 	
-	signal number_in: std_logic_vector(22 downto 0) := (others => '0');
+	signal number_in: std_logic_vector(22 downto 0) := (others => '1');
 	signal sign_out: std_logic;
 	signal exp_out: std_logic_vector(5 downto 0) := (others => '0');
 	signal mant_out: std_logic_vector(15 downto 0) := (others => '0');
@@ -26,12 +30,19 @@ architecture number_splitter_tb_arch of number_splitter_tb is
 	end component;
 
 begin
+	test_runner_setup(runner, runner_cfg);
+	logger_init(runner_trace_logger);
+
 	number_splitter_map: number_splitter port map(
 			number_in => number_in,
 			sign_out => sign_out,
 			exp_out => exp_out,
 			mant_out => mant_out
 		);
-	
+		
+	info("===EXP===" & LF & to_string(exp_out));
+	info("===MAN===" & LF & to_string(mant_out));
+	info("===SIG===" & LF & to_string(sign_out));
+
 		
 end architecture;		
