8fcd340c49099668431752743eb5bdf432a97886
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2017-04-20 17:17:01 -0300

    Update class_adder.vhd
    Added extra library

--- TP3/class_adder.vhd
+++ TP3/class_adder.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.numeric_bit_unsigned.all;
 
 entity class_adder is
      generic(N: integer:= 4);
@@ -18,7 +19,7 @@ architecture class_adder_arq of class_adder is
      signal aux: std_logic_vector(N+1 downto 0);
 
      begin
-         aux    <=     ('0' & number1_in & cin) + ('0' & number2_in & '1');
+         aux    <= ('0' & number1_in & cin) + ('0' & number2_in & '1');
          result <= aux (N downto 1);
          cout   <= aux (N+1);
 end;
