b76fcd67b53ce3e0c52ecc958940a0906446fd8c
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2017-04-20 17:27:32 -0300

    Update class_adder.vhd
    Addedd cast

--- TP3/class_adder.vhd
+++ TP3/class_adder.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.numeric_bit_unsigned.all;
 
 entity class_adder is
      generic(N: integer:= 4);
@@ -16,10 +15,10 @@ entity class_adder is
 end;
 
 architecture class_adder_arq of class_adder is
-     signal aux: std_logic_vector(N+1 downto 0);
+     signal aux: unsigned (N+1 downto 0);
 
      begin
-         aux    <= ('0' & number1_in & cin) + ('0' & number2_in & '1');
-         result <= aux (N downto 1);
+         aux    <= ('0' & unsigned(number1_in) & cin) + ('0' & unsigned(number2_in) & '1');
+         result <= std_logic_vector( aux (N downto 1) );
          cout   <= aux (N+1);
 end;
