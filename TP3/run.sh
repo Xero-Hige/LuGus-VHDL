92c93e92757524caf0ac55dd9aa6e50e7a72ea8b
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2017-04-20 18:26:55 -0300

    Update run.sh
    Enabled runner

--- TP3/run.sh
+++ TP3/run.sh
@@ -3,6 +3,8 @@ ghdl -a adder.vhd
 ghdl -a comparer.vhd
 ghdl -a binary_multiplexer.vhd
 ghdl -a class_adder.vhd
+
+python run.py
 # ghdl -a number_splitter_tb.vhd
 # ghdl -e tb
 # ghdl -r number_splitter_tb --vcd=out.vcd --stop-time=20us
