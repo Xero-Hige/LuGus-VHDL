c656c2767d0adfc0231ed75a2afa163d21e3511f
LucianoRM <luciano.rmna@gmail.com>
2017-04-27 14:43:11 -0300

    Agrego script para compilar y correr los test

--- TP3/number_splitter_tb.vhd
+++ TP3/number_splitter_tb.vhd
@@ -9,13 +9,13 @@ entity tb_standalone is
 end entity;
 
 architecture number_splitter_tb_arq of tb_standalone is
-	
+
 	signal number_in: std_logic_vector(22 downto 0) := (others => '1');
 	signal sign_out: std_logic;
 	signal exp_out: std_logic_vector(5 downto 0) := (others => '0');
 	signal mant_out: std_logic_vector(15 downto 0) := (others => '0');
 
-	
+
 	component number_splitter is
 		generic(
 			TOTAL_BITS:natural := 23;
@@ -30,8 +30,23 @@ architecture number_splitter_tb_arq of tb_standalone is
 	end component;
 
 begin
-	test_runner_setup(runner, runner_cfg);
-	logger_init(runner_trace_logger);
+	type pattern_type is record
+		 --  The inputs of the adder.
+		 i0, i1, ci : std_logic_vector;
+		 --  The expected outputs of the adder.
+		 s, co : bit;
+	end record;
+	--  The patterns to apply.
+	type pattern_array is array (natural range <>) of pattern_type;
+	constant patterns : pattern_array :=
+		(('0', '0', '0', '0', '0'),
+		 ('0', '0', '1', '1', '0'),
+		 ('0', '1', '0', '1', '0'),
+		 ('0', '1', '1', '0', '1'),
+		 ('1', '0', '0', '1', '0'),
+		 ('1', '0', '1', '0', '1'),
+		 ('1', '1', '0', '0', '1'),
+		 ('1', '1', '1', '1', '1'));
 
 	number_splitter_map: number_splitter port map(
 			number_in => number_in,
@@ -39,10 +54,10 @@ begin
 			exp_out => exp_out,
 			mant_out => mant_out
 		);
-		
+
 	info("===EXP===" & LF & to_string(exp_out));
 	info("===MAN===" & LF & to_string(mant_out));
 	info("===SIG===" & LF & to_string(sign_out));
 
-		
-end architecture;		
+
+end architecture;
