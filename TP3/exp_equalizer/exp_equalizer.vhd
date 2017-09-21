5069615390f16f04ef518bba152634bad5bfaffa
LucianoRM <luciano.rmna@gmail.com>
2017-09-21 08:14:05 -0300

    changed sensitivity list for equalizer

--- TP3/exp_equalizer/exp_equalizer.vhd
+++ TP3/exp_equalizer/exp_equalizer.vhd
@@ -7,139 +7,129 @@ use ieee.numeric_std.all;
 entity exp_equalizer is
 
 	generic(
-		TOTAL_BITS:natural := 23;
-		EXP_BITS:natural := 6
+		TOTAL_BITS : natural := 23;
+		EXP_BITS   : natural := 6
 	);
 
-	port (
-		man_1_in: in std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-		exp_1_in: in std_logic_vector(EXP_BITS-1 downto 0);
-		man_2_in: in std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-		exp_2_in: in std_logic_vector(EXP_BITS-1 downto 0);
-		man_1_out: out std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0); --extended precision
-		man_2_out: out std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0);
-		exp_out: out std_logic_vector(EXP_BITS-1 downto 0);
-		difference: out unsigned(EXP_BITS-1 downto 0)
+	port(
+		man_1_in   : in  std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
+		exp_1_in   : in  std_logic_vector(EXP_BITS - 1 downto 0);
+		man_2_in   : in  std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
+		exp_2_in   : in  std_logic_vector(EXP_BITS - 1 downto 0);
+		man_1_out  : out std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0); --extended precision
+		man_2_out  : out std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0);
+		exp_out    : out std_logic_vector(EXP_BITS - 1 downto 0);
+		difference : out unsigned(EXP_BITS - 1 downto 0)
 	);
-end;
+end exp_equalizer;
 
 architecture exp_equalizer_arq of exp_equalizer is
 
-	signal exp_1: std_logic_vector(EXP_BITS-1 downto 0);
-	signal exp_2: std_logic_vector(EXP_BITS-1 downto 0);
-	signal man_1: std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-	signal man_2: std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-	signal comparer_greater: std_logic;
-	signal comparer_smaller: std_logic;
-	signal smaller_exp: std_logic_vector(EXP_BITS-1 downto 0);
-	signal greater_exp: std_logic_vector(EXP_BITS-1 downto 0);
-	signal smaller_man: std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-	signal greater_man: std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-
-
+	signal exp_1            : std_logic_vector(EXP_BITS - 1 downto 0)              := (others => '0');
+	signal exp_2            : std_logic_vector(EXP_BITS - 1 downto 0)              := (others => '0');
+	signal man_1            : std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0) := (others => '0');
+	signal man_2            : std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0) := (others => '0');
+	signal comparer_greater : std_logic                                            := '0';
+	signal comparer_smaller : std_logic                                            := '1';
+	signal smaller_exp      : std_logic_vector(EXP_BITS - 1 downto 0)              := (others => '0');
+	signal greater_exp      : std_logic_vector(EXP_BITS - 1 downto 0)              := (others => '0');
+	signal smaller_man      : std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0) := (others => '1');
+	signal greater_man      : std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0) := (others => '1');
 
 	component comparer is
 		generic(
-			BITS:natural := 16
+			BITS : natural := 16
 		);
 
-		port (
-			number1_in: in  std_logic_vector(BITS-1 downto 0);
-			number2_in: in  std_logic_vector(BITS-1 downto 0);
-			first_greater: out std_logic;
-			second_greater: out std_logic;
-			equals: out std_logic
+		port(
+			number1_in     : in  std_logic_vector(BITS - 1 downto 0);
+			number2_in     : in  std_logic_vector(BITS - 1 downto 0);
+			first_greater  : out std_logic;
+			second_greater : out std_logic;
+			equals         : out std_logic
 		);
+
 	end component;
-	for comparer_0: comparer use entity work.comparer;
-
 
 	component binary_multiplexer is
 		generic(
-			BITS:natural := 16
+			BITS : natural := 16
 		);
-
-    port(
-	    number1_in: in  std_logic_vector(BITS-1 downto 0);
-	    number2_in: in  std_logic_vector(BITS-1 downto 0);
-
-	    chooser: in std_logic;
-
-	    mux_output: out  std_logic_vector(BITS-1 downto 0)
-    );
+		port(
+			number1_in : in  std_logic_vector(BITS - 1 downto 0);
+			number2_in : in  std_logic_vector(BITS - 1 downto 0);
+			chooser    : in  std_logic;
+			mux_output : out std_logic_vector(BITS - 1 downto 0)
+		);
 	end component;
-	for greater_exp_mux: binary_multiplexer use entity work.binary_multiplexer;
-	for smaller_exp_mux: binary_multiplexer use entity work.binary_multiplexer;
-	for greater_man_mux: binary_multiplexer use entity work.binary_multiplexer;
-	for smaller_man_mux: binary_multiplexer use entity work.binary_multiplexer;
 
-
+	for greater_exp_mux : binary_multiplexer use entity work.binary_multiplexer;
+	for smaller_exp_mux : binary_multiplexer use entity work.binary_multiplexer;
+	for greater_man_mux : binary_multiplexer use entity work.binary_multiplexer;
+	for smaller_man_mux : binary_multiplexer use entity work.binary_multiplexer;
+	for comparer_0 : comparer use entity work.comparer;
 
 begin
 
-	comparer_0: comparer
-		generic map( BITS => EXP_BITS )
-		port map (
-			first_greater => comparer_smaller,
+	comparer_0 : comparer
+		generic map(BITS => EXP_BITS)
+		port map(
+			first_greater  => comparer_smaller,
 			second_greater => comparer_greater,
-			number1_in => exp_1,
-			number2_in => exp_2
+			number1_in     => exp_1,
+			number2_in     => exp_2,
+			equals => open
 		);
 
-	greater_exp_mux: binary_multiplexer
-		generic map( BITS => EXP_BITS )
+	greater_exp_mux : binary_multiplexer
+		generic map(BITS => EXP_BITS)
 		port map(
-			chooser => comparer_greater,
+			chooser    => comparer_greater,
 			number1_in => exp_1,
 			number2_in => exp_2,
 			mux_output => greater_exp
 		);
 
-	smaller_exp_mux: binary_multiplexer
-		generic map( BITS => EXP_BITS )
+	smaller_exp_mux : binary_multiplexer
+		generic map(BITS => EXP_BITS)
 		port map(
-			chooser => comparer_smaller,
+			chooser    => comparer_smaller,
 			number1_in => exp_1,
 			number2_in => exp_2,
 			mux_output => smaller_exp
 		);
 
-	greater_man_mux: binary_multiplexer
-		generic map( BITS => TOTAL_BITS - EXP_BITS - 1)
+	greater_man_mux : binary_multiplexer
+		generic map(BITS => TOTAL_BITS - EXP_BITS - 1)
 		port map(
-			chooser => comparer_greater,
+			chooser    => comparer_greater,
 			number1_in => man_1,
 			number2_in => man_2,
 			mux_output => greater_man
 		);
-	smaller_man_mux: binary_multiplexer
-		generic map( BITS => TOTAL_BITS - EXP_BITS - 1)
-		port map (
-			chooser => comparer_smaller,
+
+	smaller_man_mux : binary_multiplexer
+		generic map(BITS => TOTAL_BITS - EXP_BITS - 1)
+		port map(
+			chooser    => comparer_smaller,
 			number1_in => man_1,
 			number2_in => man_2,
 			mux_output => smaller_man
 		);
 
-	process(man_1_in, exp_1_in, man_2_in, exp_2_in, greater_exp, smaller_exp, smaller_man, greater_man)
-		variable shifting_difference: unsigned(EXP_BITS-1 downto 0);
-		variable extended_man_greater: std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0) := (others => '0');
-		variable extended_man_smaller: std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0) := (others => '0');
+	process(man_1_in,exp_1_in,man_2_in,exp_2_in) is
+		variable shifting_difference  : unsigned(EXP_BITS - 1 downto 0)                                := (others => '0');
+		variable extended_man_greater : std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0) := (others => '0');
+		variable extended_man_smaller : std_logic_vector((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto 0) := (others => '0');
 	begin
-
-
 		exp_1 <= exp_1_in;
 		exp_2 <= exp_2_in;
 		extended_man_greater((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto (TOTAL_BITS - EXP_BITS - 1)) := greater_man;
 		extended_man_smaller((TOTAL_BITS - EXP_BITS - 1) * 2 - 1 downto (TOTAL_BITS - EXP_BITS - 1)) := smaller_man;
-		report "GM" & integer'image(to_integer(unsigned(greater_man)));
-		report "EMG" & integer'image(to_integer(unsigned(extended_man_greater)));
-		report "GE" & integer'image(to_integer(unsigned(greater_exp)));
 		shifting_difference := unsigned(greater_exp) - unsigned(smaller_exp);
-		man_1_out <= std_logic_vector(shift_right(unsigned(extended_man_smaller),to_integer(shifting_difference)));
+		man_1_out <= std_logic_vector(shift_right(unsigned(extended_man_smaller), to_integer(shifting_difference)));
 		man_2_out <= extended_man_greater;
 		exp_out <= greater_exp;
-
 	end process;
 
 end architecture;
--- TP3/exp_equalizer/exp_equalizer_tb.vhd
+++ TP3/exp_equalizer/exp_equalizer_tb.vhd
@@ -7,85 +7,81 @@ end entity;
 
 architecture exp_equalizer_tb_arq of exp_equalizer_tb is
 
-  signal man_1_in: std_logic_vector(15 downto 0);
-  signal exp_1_in: std_logic_vector(5 downto 0);
-  signal man_2_in: std_logic_vector(15 downto 0);
-  signal exp_2_in: std_logic_vector(5 downto 0);
-  signal man_1_out: std_logic_vector(31 downto 0);
-  signal man_2_out: std_logic_vector(31 downto 0);
-  signal exp_out: std_logic_vector(5 downto 0);
-  signal difference: unsigned(5 downto 0);
-
+	signal man_1_in   : std_logic_vector(15 downto 0) := (others => '0');
+	signal exp_1_in   : std_logic_vector(5 downto 0)  := (others => '0');
+	signal man_2_in   : std_logic_vector(15 downto 0) := (others => '0');
+	signal exp_2_in   : std_logic_vector(5 downto 0)  := (others => '0');
+	signal man_1_out  : std_logic_vector(31 downto 0) := (others => '0');
+	signal man_2_out  : std_logic_vector(31 downto 0) := (others => '0');
+	signal exp_out    : std_logic_vector(5 downto 0)  := (others => '0');
+	signal difference : unsigned(5 downto 0) := "000000";
 
 	component exp_equalizer is
-    generic(
-  		TOTAL_BITS:natural := 23;
-  		EXP_BITS:natural := 6
-  	);
+		generic(
+			TOTAL_BITS : natural := 23;
+			EXP_BITS   : natural := 6
+		);
 
-  	port (
-  		man_1_in: in std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-  		exp_1_in: in std_logic_vector(EXP_BITS-1 downto 0);
-  		man_2_in: in std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
-  		exp_2_in: in std_logic_vector(EXP_BITS-1 downto 0);
-      man_1_out: out std_logic_vector((TOTAL_BITS - EXP_BITS - 2) * 2 + 1 downto 0); --extended precision
-  		man_2_out: out std_logic_vector((TOTAL_BITS - EXP_BITS - 2) * 2 + 1 downto 0);
-  		exp_out: out std_logic_vector(EXP_BITS-1 downto 0);
-      difference: out unsigned(EXP_BITS-1 downto 0)
-  	);
-  end component;
-	for exp_equalizer_0: exp_equalizer use entity work.exp_equalizer;
+		port(
+			man_1_in   : in  std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
+			exp_1_in   : in  std_logic_vector(EXP_BITS - 1 downto 0);
+			man_2_in   : in  std_logic_vector(TOTAL_BITS - EXP_BITS - 2 downto 0);
+			exp_2_in   : in  std_logic_vector(EXP_BITS - 1 downto 0);
+			man_1_out  : out std_logic_vector((TOTAL_BITS - EXP_BITS - 2) * 2 + 1 downto 0); --extended precision
+			man_2_out  : out std_logic_vector((TOTAL_BITS - EXP_BITS - 2) * 2 + 1 downto 0);
+			exp_out    : out std_logic_vector(EXP_BITS - 1 downto 0);
+			difference : out unsigned(EXP_BITS - 1 downto 0)
+		);
+	end component;
+	for exp_equalizer_0 : exp_equalizer use entity work.exp_equalizer;
 
 begin
 
-	exp_equalizer_0: exp_equalizer
-    generic map(TOTAL_BITS => 23, EXP_BITS => 6)
-    port map(
-			exp_1_in => exp_1_in,
-      exp_2_in => exp_2_in,
-      man_1_in => man_1_in,
-      man_2_in => man_2_in,
-      exp_out => exp_out,
-      man_1_out => man_1_out,
-      man_2_out => man_2_out,
-      difference => difference
-
+	exp_equalizer_0 : exp_equalizer
+		generic map(TOTAL_BITS => 23, EXP_BITS => 6)
+		port map(
+			exp_1_in   => exp_1_in,
+			exp_2_in   => exp_2_in,
+			man_1_in   => man_1_in,
+			man_2_in   => man_2_in,
+			exp_out    => exp_out,
+			man_1_out  => man_1_out,
+			man_2_out  => man_2_out,
+			difference => difference
 		);
 
 	process
 		type pattern_type is record
-			 m1 : std_logic_vector(15 downto 0);
-			 e1 : std_logic_vector(5 downto 0);
-			 m2 : std_logic_vector(15 downto 0);
-			 e2 : std_logic_vector(5 downto 0);
-       mo1: std_logic_vector(31 downto 0);
-       mo2: std_logic_vector(31 downto 0);
-       eo: std_logic_vector(5 downto 0);
+			m1  : std_logic_vector(15 downto 0);
+			e1  : std_logic_vector(5 downto 0);
+			m2  : std_logic_vector(15 downto 0);
+			e2  : std_logic_vector(5 downto 0);
+			mo1 : std_logic_vector(31 downto 0);
+			mo2 : std_logic_vector(31 downto 0);
+			eo  : std_logic_vector(5 downto 0);
 		end record;
 		--  The patterns to apply.
-		type pattern_array is array (natural range<>) of pattern_type;
+		type pattern_array is array (natural range <>) of pattern_type;
 		constant patterns : pattern_array := (
-      ("0000000000000001", "111111", "0000000000000000", "111111","00000000000000010000000000000000","00000000000000000000000000000000","111111"),
-      ("0000000000000001", "000001", "0000000000000001", "000000","00000000000000010000000000000000","00000000000000001000000000000000","000001")
-
+			("0000000000000001", "000000", "0000000000000001", "000000", "00000000000000010000000000000000", "00000000000000010000000000000000", "000000"),
+			("0000000000000001", "111110", "0000000000000000", "111111", "00000000000000010000000000000000", "00000000000000000000000000000000", "111111")
 		);
 
-		begin
+	begin
+		for i in patterns'range loop
+			--  Set the inputs.
+			exp_1_in <= patterns(i).e1;
+			exp_2_in <= patterns(i).e2;
+			man_1_in <= patterns(i).m1;
+			man_2_in <= patterns(i).m2;
 
-	  for i in patterns'range loop
-	     --  Set the inputs.
-	     exp_1_in <= patterns(i).e1;
-       exp_2_in <= patterns(i).e2;
-       man_1_in <= patterns(i).m1;
-       man_2_in <= patterns(i).m2;
+			wait for 100 ms;
 
-	     wait for 100 ms;
-
-       assert patterns(i).mo1 = man_1_out report "BAD MANTISSA 1, GOT: " & integer'image(to_integer(unsigned(man_1_out)));
-       assert patterns(i).mo2 = man_2_out report "BAD MANTISSA 2, GOT: " & integer'image(to_integer(unsigned(man_2_out)));
-       assert patterns(i).eo = exp_out report "BAD EXP, GOT: " & integer'image(to_integer(unsigned(exp_out)));
-	     --  Check the outputs.
-    end loop;
+			assert patterns(i).mo1 = man_1_out report "BAD MANTISSA 1, GOT: " & integer'image(to_integer(signed(man_1_out)));
+			assert patterns(i).mo2 = man_2_out report "BAD MANTISSA 2, GOT: " & integer'image(to_integer(signed(man_2_out)));
+			assert patterns(i).eo = exp_out report "BAD EXP, GOT: " & integer'image(to_integer(signed(exp_out)));
+			--  Check the outputs.
+		end loop;
 		assert false report "end of test" severity note;
 		wait;
 	end process;
